981253d27e15c0e799c3739778f4e85772549d81
moid1 <mueedkhan766@gmail.com>
2021-12-29 06:52:52 +0500

    done with the 3rd milestone

--- content/store/includes/config.php
+++ content/store/includes/config.php
@@ -1,8 +1,11 @@
 <?php
 $dbhost_name = "localhost";
-$database = "php";
-$username = "root";
-$password = "admin123";
+// $database = "php";
+// $username = "root";
+// $password = "admin123";
+$database = "virtual6_wms";
+$username = "virtual6_wms";
+$password = "virtual6_wms";
 try {
 $dbo = new PDO("mysql:host=".$dbhost_name.";dbname=".$database, $username, $password);
 } catch (PDOException $e) {
