ab760b81034df02c28986255b6c1347f53977cb4
Ronio Jerico Takahashi <roniojericho144@yahoo.com>
2023-07-26 17:36:13 +0800

    config

--- content/store/includes/db_connect.php
+++ content/store/includes/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_HOST", "localhost");
 define("DB_USER", "u195495796_root");
-define("DB_PASS", "Admin123*");
+define("DB_PASS", "admin123");
 define("DB_NAME", "u195495796_wms");
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($db->connect_errno > 0) {
