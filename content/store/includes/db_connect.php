2382735c3d30481d2a29e7be025ad7b2b7554da9
Ronio Jerico Takahashi <roniojericho144@yahoo.com>
2023-07-27 01:14:55 +0800

    Set new database values

--- content/store/includes/db_connect.php
+++ content/store/includes/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "u195495796_root");
-define("DB_PASS", "Admin123*");
-define("DB_NAME", "u195495796_wms");
+define("DB_USER", "virtual6_wms");
+define("DB_PASS", "virtual6_wms");
+define("DB_NAME", "virtual6_wms");
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($db->connect_errno > 0) {
     die("Unable to connect to databasee [" . $db->connect_error . "]");
