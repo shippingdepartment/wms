ce373e287484976462f425b19e845712ff0624f2
Ronio Jerico Takahashi <roniojericho144@yahoo.com>
2023-08-01 18:57:37 +0800

    db_connect credentials

--- content/store/includes/db_connect.php
+++ content/store/includes/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 define ("DB_HOST", "localhost");
-define ("DB_USER", "root");
-define ("DB_PASS", "123");
-define ("DB_NAME", "php");
+define ("DB_USER", "u195495796_root");
+define ("DB_PASS", "Admin123*");
+define ("DB_NAME", "u195495796_wms");
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if($db->connect_errno > 0){
 die("Unable to connect to database [".$db->connect_error."]");
