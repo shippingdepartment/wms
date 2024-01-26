a0a884eeb5d1b6d23b1f6801de0a17e1ea50ad47
Ronio Jerico Takahashi <roniojericho144@yahoo.com>
2024-01-26 23:17:57 +0800

    chaanged db_con

--- content/store/includes/db_connect.php
+++ content/store/includes/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 define ("DB_HOST", "localhost");
-define ("DB_USER", "u195495796_root");
-define ("DB_PASS", "Admin123*");
+define ("DB_USER", "u195495796_cody");
+define ("DB_PASS", "Adminn123*");
 define ("DB_NAME", "u195495796_wmsdemo");
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if($db->connect_errno > 0){
