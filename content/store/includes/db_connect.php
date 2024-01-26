450a9ece48f0df8316148a01490628d5ee834662
Ronio Jerico Takahashi <roniojericho144@yahoo.com>
2024-01-26 23:14:26 +0800

    change db name

--- content/store/includes/db_connect.php
+++ content/store/includes/db_connect.php
@@ -2,7 +2,7 @@
 define ("DB_HOST", "localhost");
 define ("DB_USER", "u195495796_root");
 define ("DB_PASS", "Admin123*");
-define ("DB_NAME", "u195495796_wms");
+define ("DB_NAME", "u195495796_wmsdemo");
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if($db->connect_errno > 0){
 die("Unable to connect to database [".$db->connect_error."]");
