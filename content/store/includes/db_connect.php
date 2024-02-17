2e3113e360a2c573dd0925fb1051bfb1755fc848
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-17 07:39:29 -0500

    Use .env file to get database constants

--- content/store/includes/db_connect.php
+++ content/store/includes/db_connect.php
@@ -1,10 +1,12 @@
 <?php
-define ("DB_HOST", "localhost");
-define ("DB_USER", "u195495796_cody");
-define ("DB_PASS", "Adminn123*");
-define ("DB_NAME", "u195495796_wmsdemo");
-$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if($db->connect_errno > 0){
-die("Unable to connect to database [".$db->connect_error."]");
+
+$env = parse_ini_file("../../.env");
+
+foreach ($env as $key => $value) {
+    define($key, $value);
 }
-?>
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($db->connect_errno > 0) {
+    die("Unable to connect to database [" . $db->connect_error . "]");
+}
